d6eaa7dae15df1c38d6248ed6b2dbabb01d97819
Hossein Lotfypoor <148619164+Hossein-lotfypoor@users.noreply.github.com>
2026-06-03 21:34:29 +0330

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Workshop-Ledger/sw.js', { scope: '/Workshop-Ledger/' })})}
